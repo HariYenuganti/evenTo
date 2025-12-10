59e385e1c838511edd10d2d83d0c51386ca6a406
HariYenuganti <haripranay07@gmail.com>
2025-12-10 12:44:45 -0500

    feat: add Logo and H1 components

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 export default function Logo() {
   return (
-    <Image
-      src="https://bytegrad.com/course-assets/react-nextjs/evento.png"
-      alt="EVENTO"
-      width={53}
-      height={12}
-    />
+    <Link href="/">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/evento.png"
+        alt="EVENTO"
+        width={53}
+        height={12}
+      />
+    </Link>
   );
 }
